24e831df0bc79daee479db38bb6f5acea3ba02d0
Sanji on Eth <sanjioneth@yahoo.com>
2025-06-05 21:29:19 -0400

    Update frontend for SANJI mint cooldowns and remaining counters 13

--- sanji-dapp/hooks/useSanjiMint.js
+++ sanji-dapp/hooks/useSanjiMint.js
@@ -3,7 +3,7 @@ import { ethers } from "ethers";
 import BaseDeck from "../contracts/BaseDeckNFT.json";
 import ERC20 from "../contracts/erc20.json";
 
-const BASE_DECK_ADDRESS = "0x169d50acDfDe126776e969963343523e2d543283";
+const BASE_DECK_ADDRESS = "0x6383d2CB2a9e17a95548917805104A181ac58E3D";
 const SANJI_ADDRESS = "0x8E0B3E3Cb4468B6aa07a64E69DEb72aeA8eddC6F";
 const SANJI_REQUIRED = ethers.parseUnits("1000000", 18);
 const COOLDOWN = 365 * 24 * 60 * 60;
